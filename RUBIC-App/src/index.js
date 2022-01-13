bcf5b07840a122288356df03907cd76433a7c56e
chrisAXZA <nexter1@tutanota.com>
2022-01-13 12:11:19 +0200

    Add handlebars

--- RUBIC-App/src/index.js
+++ RUBIC-App/src/index.js
@@ -1,1 +1,24 @@
-console.log('Started');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const { engine } = require('express-handlebars');
+const path = require('path');
+// app.engine('handlebars', engine());
+// app.set('view engine', 'handlebars');
+
+const app = express();
+
+app.set('views', path.resolve('./src/views'));
+// console.log(path.resolve('./src/views'));
+app.engine('hbs', engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.all('/', (req, res) => {
+    // res.write('Hello World');
+    // res.end();
+    res.render('index', {layout: false});
+});
+
+app.listen(5000, console.log.bind(console, 'Application is runnging on http://localhost:5000'));
