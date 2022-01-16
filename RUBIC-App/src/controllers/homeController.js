879d6234788521c157a0b1085cb7da2b189b173a
chrisAXZA <nexter1@tutanota.com>
2022-01-16 21:13:41 +0200

    Implement partial cube

--- RUBIC-App/src/controllers/homeController.js
+++ RUBIC-App/src/controllers/homeController.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cubeService = require('../services/cubeService.js');
 
 const router = express.Router();
 
-const renderHome = (req, res)=>{
-    res.render('index');
+const renderHome = (req, res) => {
+    let cubes = cubeService.getAll();
+
+    res.render('index', {
+        cubes
+    });
 };
 
 router.get('/', renderHome);
--- RUBIC-App/src/models/Cube.js
+++ RUBIC-App/src/models/Cube.js
@@ -1,14 +1,25 @@
+const uniqid = require('uniqid');
+
 class Cube {
     static #cubes = [
         {
+            id: 'sfhdiofehruioehcn',
             name: 'Mirror Cube',
             description: 'Rubic Cube with mirrors',
             imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Mirror_Cube_solved.png/220px-Mirror_Cube_solved.png',
             difficulty: '4'
+        },
+        {
+            id: '1geqjtgwfkyhmdpm9',
+            name: 'Ice Cube',
+            description: 'Some cold ice cube',
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Iced_tea_with_ice_cubes.jpg/220px-Iced_tea_with_ice_cubes.jpg',
+            difficulty: '1'
         }
     ];
 
     constructor(name, description, imageUrl, difficulty) {
+        this.id = uniqid(); // sets new id 
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
