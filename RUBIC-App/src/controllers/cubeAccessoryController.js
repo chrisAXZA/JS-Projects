09bce076ecd483ad8fa21231dd222c8168055901
chrisAXZA <nexter1@tutanota.com>
2022-01-23 15:40:15 +0200

    Show accessories on accessory attach page

--- RUBIC-App/src/controllers/cubeAccessoryController.js
+++ RUBIC-App/src/controllers/cubeAccessoryController.js
@@ -1,14 +1,15 @@
 const router = require('express').Router({ mergeParams: true });
 
 const cubeService = require('../services/cubeService.js');
+const accessoryService = require('../services/accessoryService.js');
 
 router.get('/add', async (req, res) => {
     let cube = await cubeService.getOne(req.params.cubeId);
-
+    let accessories = await accessoryService.getAll();
 
     // console.log(req.params.cubeId);
     // res.render('cube/accessory/add', cube);
-    res.render('cube/accessory/add', { ...cube });
+    res.render('cube/accessory/add', { cube, accessories });
 
     // res.end();
 });
--- RUBIC-App/src/services/accessoryService.js
+++ RUBIC-App/src/services/accessoryService.js
@@ -5,8 +5,13 @@ async function create(name, description, imageUrl) {
     return Accessory.create({ name, description, imageUrl });
 }
 
+async function getAll() {
+    return Accessory.find({}).lean();
+}
+
 const accessoryService = {
     create,
+    getAll,
 };
 
 // Alternative to module exports
