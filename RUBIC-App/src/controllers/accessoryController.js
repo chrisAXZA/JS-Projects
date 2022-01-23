396f827df74eb11b035c4e81f39da48ba82460fa
chrisAXZA <nexter1@tutanota.com>
2022-01-23 12:22:04 +0200

    Add create accessory action

--- RUBIC-App/src/controllers/accessoryController.js
+++ RUBIC-App/src/controllers/accessoryController.js
@@ -1,13 +1,17 @@
 const router = require('express').Router();
+const accessoryService = require('../services/accessoryService.js')
 
-router.get('/create', (req, res)=>{
-        res.render('accessory/create');
+// console.log(create);
+
+router.get('/create', (req, res) => {
+    res.render('accessory/create');
 });
 
-router.post('/create', (req, res)=>{
-    let accessory = req.body;
-    
-    console.log(accessory);
+router.post('/create', async (req, res) => {
+    let { name, description, imageUrl } = req.body;
+    let accessory = await accessoryService.create(name, description, imageUrl);
+
+    // console.log(accessory);
 
     res.redirect('/');
 });
