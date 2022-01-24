0bd6f8a4063bb03a22bb680b8bfeeefc53116633
chrisAXZA <nexter1@tutanota.com>
2022-01-24 16:44:12 +0200

    Add register page

--- RUBIC-App/src/controllers/authController.js
+++ RUBIC-App/src/controllers/authController.js
@@ -10,4 +10,9 @@ router.post('/login', (req, res) => {
    res.redirect('/login');
 });
 
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+
 module.exports = router;
