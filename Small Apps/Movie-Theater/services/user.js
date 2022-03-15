9e2c86b2e54be46b22780997edbb944aa72b9698
chrisAXZA <nexter1@tutanota.com>
2022-03-15 08:19:41 +0200

    Upload initial code to small apps

---
+++ Small Apps/Movie-Theater/services/user.js
@@ -0,0 +1,52 @@
+const User = require('../models/User.js');
+const { hash, compare } = require('bcrypt');
+
+async function register(username, password) {
+    const existing = await getUserByUsername(username);
+    // console.log(existing);
+
+    if (existing) {
+        throw new Error('Username is taken!');
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const user = new User({
+        username,
+        hashedPassword,
+    });
+
+    await user.save();
+
+    return user;
+}
+
+async function login(username, password) {
+    const user = await getUserByUsername(username);
+
+    if (!user) {
+        // throw new Error('User does not exist!');
+        throw new Error('Incorrect username or password!');
+    }
+
+    const hasMatch = await compare(password, user.hashedPassword);
+
+    if (!hasMatch) {
+        throw new Error('Incorrect username or password!');
+    }
+
+    return user;
+}
+
+async function getUserByUsername(username) {
+    const user = User.findOne({
+        username: new RegExp(`^${username}$`, 'i'),
+    });
+
+    return user;
+}
+
+module.exports = {
+    login,
+    register,
+};
