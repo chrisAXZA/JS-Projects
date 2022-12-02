3aefb26ee12117db7a6cdfdd375292153683affe
chrisAXZA <nexter1@tutanota.com>
2022-12-02 13:27:51 +0200

    Add Fibonacci.js in Basic Algorithms

---
+++ Basic Algorithms/Fibonacci.js
@@ -0,0 +1,34 @@
+// Big O: O(n), as n increases as the number of times the given
+// loop executes proportionally 
+
+function fibonacci(n) {
+    let num1 = 0;
+    let num2 = 1;
+    let finalArr = [num1, num2];
+    let start = finalArr.length;
+
+    for (let i = start; i < n; i++) {
+        let num = finalArr[i - 2] + finalArr[i - 1];
+        finalArr.push(num);
+    }
+
+    console.log(finalArr);
+    // finalArr.forEach((el) => {
+    //     console.log('*'.repeat(el));
+    // });
+}
+
+// fibonacci(2);
+// fibonacci(5);
+// fibonacci(7);
+// fibonacci(22);
+
+function fibonacci2(n) {
+    const fib = [0, 1];
+
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib;
+}
