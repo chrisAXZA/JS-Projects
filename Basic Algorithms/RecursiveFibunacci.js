7bf913f838a95f5a35b93af0540c7fb4152a971f
chrisAXZA <nexter1@tutanota.com>
2022-12-05 18:58:27 +0200

    Add RecursiveFibunacci.js in Basic Algorithms

---
+++ Basic Algorithms/RecursiveFibunacci.js
@@ -0,0 +1,22 @@
+// Big O(n) in iterative solution, in recursive solution Big O(2^n)
+// resulting in extremely poor performance> Recursion is poor implementation
+// for Fibonacci
+
+// n denotes element position in the Fibonacci sequence, n starts at 0
+function recursiveFibonacci(n) {
+    // Fibonacci sequence is a sequence in which each number is the sum of the two
+    // preceding ones. If F represents a funcation to calculate the Fibonacci 
+    // number, then: Fn =Fn-1 + Fn-2 . Each number is the sum of the previous 2 numbers.
+
+    if (n < 2) {
+        // returns 0 if n = 0, or 1 if n = 1
+        return n;
+    }
+
+    return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
+
+// console.log(recursiveFibonacci(0));
+// console.log(recursiveFibonacci(1));
+console.log(recursiveFibonacci(2));
+console.log(recursiveFibonacci(6));
