16862db22e06af4aea7ff67a73a5f8d4e2c504f2
chrisAXZA <nexter1@tutanota.com>
2022-12-05 21:00:45 +0200

    Add LinearSearch.js in Basic Algorithms

---
+++ Basic Algorithms/LinearSearch.js
@@ -0,0 +1,19 @@
+// Big O(n), linear - as the size of the array increases,
+// the number of operations increases accordingly
+
+function linearSearch(arr, target) {
+    // given an array of n elements and a target element t, fint the index
+    // of t in the array. Return -1 if the target element is not found
+
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+console.log(linearSearch([-5, 2, 10, 4, 6], 10));
+console.log(linearSearch([-5, 2, 10, 4, 6], 6));
+console.log(linearSearch([-5, 2, 10, 4, 6], 20));
